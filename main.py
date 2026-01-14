34d334fce058e958bc4168a116c5162c53e91c53
shukurulloh <madrahimovq@gmail.com>
2026-01-14 22:40:23 +0500

    1-topshiriq

--- main.py
+++ main.py
@@ -2,10 +2,16 @@
 # numbers = [1,2,3,4,5,6,7,8,9,10]
 # num_res = list(map(lambda n: n * 3, numbers))
 # print(num_res)
-
+from wsgiref.validate import validator
 
 #_#_#_#_#_#_#_#_#_#_ Topshiriq__2
 # numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 # k_num = list(map(lambda value: value**2,numbers))
 #
 # print(k_num)
+
+#_#_#_#_#_#_#_#_#_#_ Topshiriq__3
+# words = ["salom", "alik", "abdefgh"]
+# res_word = list(map(lambda value: value.upper(),words))
+#
+# print(res_word)
