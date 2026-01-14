c60e10744ee7d4c9a1750603e53a7bc00aea7034
shukurulloh <madrahimovq@gmail.com>
2026-01-14 22:20:18 +0500

    ikkinchisi yuklandi

--- main.py
+++ main.py
@@ -3,4 +3,9 @@
 # num_res = list(map(lambda n: n * 3, numbers))
 # print(num_res)
 
+
 #_#_#_#_#_#_#_#_#_#_ Topshiriq__2
+# numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# k_num = list(map(lambda value: value**2,numbers))
+#
+# print(k_num)
