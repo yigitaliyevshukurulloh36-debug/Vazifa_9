e4280c911b82cd1d83ca2de41c0da463cab92f19
shukurulloh <madrahimovq@gmail.com>
2026-01-15 10:32:42 +0500

    barcha topshiriqlar bajarilda hammasini commit qilmoxchi edim kop bo'lar ekan shuning uchun qisimlarga bo'lib chiqdim😁🙋‍♂️🙋‍♂️

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # num_res = list(map(lambda n: n * 3, numbers))
 # print(num_res)
 from importlib.metadata import pass_none
+from tkinter.font import names
 from wsgiref.validate import validator
 
 #_#_#_#_#_#_#_#_#_#_ Topshiriq__2
@@ -48,3 +49,36 @@ from wsgiref.validate import validator
 # number_list = [1,2,3,4,5,6,7,8,9,10 ]
 # res_n_l = list(filter(lambda value: value % 2 == 0, number_list))
 # print(res_n_l)
+
+#_#_#_#_#_#_#_#_#_#_ Topshiriq__10
+# numbers_list = [1,-2,3,-4,5,-6,7,-8,9,-10]
+# num_res_list = list(filter(lambda value: value>0, numbers_list))
+
+# print(num_res_list)
+
+#_#_#_#_#_#_#_#_#_#_ Topshiriq__11
+# words_list = ["hello my friend", "hi my friend", "hi my love", "what "]
+# words_list_res = list(filter(lambda value: len(value)>5, words_list))
+#
+# print(words_list_res)
+
+#_#_#_#_#_#_#_#_#_#_ Topshiriq__12
+# numbers_list = [1,-2,3,-4,5,-6,7,-8,9,10]
+# numbers_list_res = list(filter(lambda value: value%5==0, numbers_list))
+# print(numbers_list_res)
+
+#_#_#_#_#_#_#_#_#_#_ Topshiriq__13
+# name_list = ["Ali", "Vali", "Jasur", "Asilbek"]
+# names_res = list(filter(lambda value: value[0]=="A", name_list))
+# print(names_res)
+
+#_#_#_#_#_#_#_#_#_#_ Topshiriq__14
+# numbers_list = [1,-2,3,-4,5,-6,0,7,-8,9,10]
+# res_num_list = list(filter(lambda value: value>=0, numbers_list))
+# print(res_num_list)
+
+#_#_#_#_#_#_#_#_#_#_ Topshiriq__15
+# words_list = ["hello my friend", "my friend python", "my love python", "what "]
+# words_list_res = list(filter(lambda value: "python" in value, words_list))
+# print(words_list_res)
+
